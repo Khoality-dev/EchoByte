e1079d3c9347840b89942b0391a8ef39fb359f4f
Khoa <anhkhoan@sfu.ca>
2024-03-16 12:52:54 -0700

    add queue

--- main.py
+++ main.py
@@ -7,63 +7,74 @@ import yt_dlp
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+is_playing = False
+list_urls = []
+
+def handle_music_done(voice_client):
+    if (len(list_urls) == 0):
+        return
+    
+    list_urls.pop(0)
+
+    handle_play_music(voice_client)
+
+def handle_play_music(voice_client):
+    if (len(list_urls) == 0):
+        return
+    
+    url = list_urls[0]
+
+    voice_client.play(discord.FFmpegPCMAudio(url), after=lambda e: handle_music_done(voice_client))
+
 # Event: Bot is ready
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
-# Command: Ping
+# Function to play music
 @bot.command()
-async def ping(ctx):
-    await ctx.send('Pong!')
-
-# Command: Echo
-@bot.command()
-async def echo(ctx, *, message):
-    await ctx.send(message)
-
-# Command: Join
-@bot.command()
-async def join(ctx):
-    # Check if the command invoker is in a voice channel
-    if ctx.author.voice:
-        # Connect to the voice channel of the command invoker
-        voice_channel = ctx.author.voice.channel
-        await voice_channel.connect()
-    else:
-        await ctx.send("You need to be in a voice channel to use this command.")
-
-# Function to play audio from a YouTube link
-@bot.command()
-async def play_youtube(ctx, url):
+async def play(ctx, url):
     voice_channel = ctx.author.voice.channel
-    if voice_channel is None:
-        await ctx.send("You need to be in a voice channel to use this command.")
-        return
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
 
-    voice_client = await voice_channel.connect()
+    if not voice_client is None: #test if voice is None
+        if not voice_client.is_connected():
+            await voice_channel.connect()
+    else:
+        await voice_channel.connect()
+    
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
 
     # Use youtube_dl to extract the audio stream
     ydl_opts = {
         'format': 'bestaudio/best',
         'extractaudio': True,
     }
+
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=False)
-        voice_client.play(discord.FFmpegPCMAudio(info['url'] ), after=lambda e: print('done', e))
+        url = info['url']
 
-
-# Function to play music
+    list_urls.append(url)
+    if (len(list_urls) == 1):
+        handle_play_music(voice_client)
+    
 @bot.command()
-async def play(ctx, path):
-    voice_channel = ctx.author.voice.channel
-    if voice_channel is None:
-        await ctx.send("You need to be in a voice channel to use this command.")
+async def skip(ctx):
+    
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+
+    if not voice_client is None: #test if voice is None
+        if not voice_client.is_connected():
+            return
+    else:
         return
+    
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
 
-    voice_client = await voice_channel.connect()
-    voice_client.play(discord.FFmpegPCMAudio(path))  
+    voice_client.stop()
 
+    handle_music_done(voice_client)
 
 # Run the bot
 bot.run(DISCORD_API_KEY)
